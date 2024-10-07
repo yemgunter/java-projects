e1ddf59a03147e3c6ec2d694e7fbe7cb990e8fa6
Yolanda Gunter <yemgunter@gmail.com>
2024-10-06 21:04:12 -0500

    Update Proj02.java
    Rename class

--- MLO-1.2/Proj02.java
+++ MLO-1.2/Proj02.java
@@ -1,4 +1,4 @@
-class Proj01 {
+class Proj02 {
     public static void main(String[] args) {
         System.out.println("I certify that this program is my own work");
         System.out.println("and is not the work of others. I agree not");
