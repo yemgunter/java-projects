049e6a1ed3ac48ea6b709a332cf1bd0ed8a37592
Yolanda Gunter <yemgunter@gmail.com>
2024-10-12 22:14:52 -0500

    Reset to original Proj03Runner file
    Reset changes to original Proj03Runner file to recompile and get first error.
    
    Compiled, reviewed first error, modify code to eliminate error.

--- MLO-1.3/Proj03Runner.java
+++ MLO-1.3/Proj03Runner.java
@@ -1,22 +1,18 @@
+class Proj03Runner {
 
-
-class Proj03Runner{
-
-  static int run(int valIn){//receive value as type int
-    //Print a required certification on the 
+  static int run(int valIn) {// receive value as type int
+    // Print a required certification on the
     // command-line screen.
     System.out.println(
-      "I certify that this program is my own work \n"+
-      "and is not the work of others. I agree not \n" +
-      "to share my solution with others.\n" +
-      "Yolanda Gunter \n");
+        "I certify that this program is my own work \n" +
+            "and is not the work of others. I agree not \n" +
+            "to share my solution with others.\n" +
+            "Yolanda Gunter \n");
 
+    byte myVar = (byte) valIn;// access and store valIn as type byte
+    System.out.println(myVar);// print it
+    myVar = --myVar;// decrement it
+    return myVar;// return it
+  }// end run
 
-
-    byte myVar = (byte) valIn;//access and store valIn as type byte
-    System.out.println(myVar);//print it
-    myVar = ++myVar;//increment it
-    return myVar;//return it
-  }//end run
-
-}//end class Proj03Runner
+}// end class Proj03Runner
