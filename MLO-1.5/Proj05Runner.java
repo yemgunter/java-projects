b5ae5822fdbe4314c3255a9a69860bc1c6cdaacd
Yolanda Gunter <yemgunter@gmail.com>
2024-10-27 22:52:08 -0500

    Create Proj05Runner.java
    Borrowed Proj04Skeleton code for revisions to continue Proj05 specifications

---
+++ MLO-1.5/Proj05Runner.java
@@ -0,0 +1,57 @@
+/* File Proj05Runner
+This is borrowed code from Proj04. After I revise it
+It will compile and run, to meet the assignment specifications.
+***********************************************************(***/
+
+class Proj05Runner {
+
+    static double run(byte[] byteArray) {
+        // Print a required certification on the
+        // command-line screen.
+        System.out.println(
+                "I certify that this program is my own work \n" +
+                        "and is not the work of others. I agree not \n" +
+                        "to share my solution with others.\n" +
+                        "Yolanda Gunter\n");
+
+        // Insert code to get and display the length of the array
+        int arrayLength = byteArray.length;
+        System.out.println("Array length = " + arrayLength);
+
+        // Insert code to get and display the value in the
+        // last element in the array.
+        int lastElement_val = byteArray[arrayLength - 1];
+        System.out.println("Value in last element = " + lastElement_val);
+
+        // Insert code to determine and display whether the
+        // last element in the array is odd or even.
+        if (lastElement_val % 2 == 0) {
+            System.out.println("The Value is the last element is Even");
+        } else {
+            System.out.println("The value in the last element is Odd");
+        }
+
+        double sum = 0;
+        // Insert code to compute and display the sum of all the
+        // values stored in the array as type double. Include
+        // code that will display the beginning and ending
+        // indices.
+        System.out.println("Begin loop at index = 0");
+        System.out.println("End loop at index = " + (arrayLength - 1));
+
+        for (int i = 0; i < arrayLength; i++) {
+            sum += byteArray[i];
+        }
+
+        System.out.println("The sum is " + sum);
+
+        double avg = sum / arrayLength;
+        // Insert code to compute and display the average of all
+        // the values in the array.
+        System.out.println("The average is " + avg);
+
+        return avg;
+
+    }// end run method
+
+}// end class Proj04Runner
