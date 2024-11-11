a4c623da205c95fadfeca89b47e6dd584080f4e7
Yolanda Gunter <yemgunter@gmail.com>
2024-11-10 23:23:18 -0600

    Create Proj07Runner,java
    Follow client scope - clean up line spacing.

---
+++ MLO-1.7/Proj07Runner.java
@@ -0,0 +1,16 @@
+class Proj07Runner extends Proj07 {
+
+    public Proj07Runner() {
+
+	System.out.println(
+        	"I certify that this program is my own work \n" +
+                "and is not the work of others. I agree not \n" +
+                "to share my solution with others.");
+    }//end constructor
+
+    public void run() {
+        System.out.println(
+		"Yolanda Gunter \n");
+    }// end run
+}// end class Proj07Runner
+
