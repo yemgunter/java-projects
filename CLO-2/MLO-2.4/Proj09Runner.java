d1511b5c1890688d0be8d2c3546d2696bb76087d
Yolanda Gunter <yemgunter@gmail.com>
2024-11-25 05:29:36 -0600

    Create Proj09Runner.java

--- CLO-2/MLO-2.4/Proj09Runner.java
+++ CLO-2/MLO-2.4/Proj09Runner.java
@@ -1,8 +1,8 @@
-public class Proj09Runner {
-    byte valA;
+public class Proj09Runner extends Object {
+    byte valA; // Instance variable to store byte value
 
-    public Proj09Runner(byte valA) {
-        this.valA = valA;
+    public Proj09Runner(byte valA) { // Construtor initalizing value
+        this.valA = valA; // Save value into the instance value
 
         System.out.println("I certify that this program is my own work");
         System.out.println("and is not the work of others. I agree not");
@@ -10,8 +10,7 @@ public class Proj09Runner {
         System.out.println("Yolanda Gunter \n");
     }
 
-    @Override
-    public String toString() {
-        return "Class Proj09Runner " + valA;
+    public String toString() { // Overriding toString method
+        return "Class Proj09Runner " + valA; // Return string
     } // end constructor
 } // end class
