7d514e496eff219c2b33f3dac08810d2ff1e5300
Yolanda Gunter <yemgunter@gmail.com>
2024-12-01 23:52:19 -0600

    Update Proj10X.java

--- CLO-2/MLO-2.5/Proj10X.java
+++ CLO-2/MLO-2.5/Proj10X.java
@@ -1,4 +1,16 @@
 public interface Proj10X {
-    int getData();            // Returns original randomData
-    int getModifiedData();    // Returns modified data
-}
+
+    default void printCertification() {
+        System.out.println(
+                "I certify that this program is my own work\n" +
+                        "and is not the work of others. I agree not\n" +
+                        "to share my solution with others.\n" +
+                        "Yolanda\n" +
+                        "Gunter");
+    }// end printCertification
+
+    int getData(); // Returns original randomData
+
+    int getModifiedData(); // Returns modified data
+
+}// end interface Proj10X
