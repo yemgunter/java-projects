49b0a4e951cf089b06d714925d383f1d1bbf66c5
Yolanda Gunter <yemgunter@gmail.com>
2024-12-09 08:45:48 -0600

    Update Proj11Runner.java
    Copied loops from driver then updated array for row and column

--- CLO-2/MLO-2.6/Proj11Runner.java
+++ CLO-2/MLO-2.6/Proj11Runner.java
@@ -1,15 +1,21 @@
 public class Proj11Runner {
 
     public void run(Object[][] array) {
+
+        System.out.println(
+                "I certify that this program is my own work\n" +
+                        "and is not the work of others. I agree not\n" +
+                        "to share my solution with others.\n" +
+                        "Yolanda Gunter\n"); // end Certification
+
         int count = 1; // start with 1
 
         // began loop for each row (lines 41-48 Proj11.java)
-        for (int i = 0; i < 3; i++) { // line 42
+        for (int i = 0; i < array.length; i++) { // line 42
 
             // began loop for each column in those rows
-            for (int j = 0; j < vals[i]; j++) { // line 43
-                array[i][j] + " "; // line 44
-
+            for (int j = 0; j < array[i].length; j++) { // line 43
+                array[i][j] = count++; // line 44
             } // end loop for column
         } // end loop for row
     } // end method for array
