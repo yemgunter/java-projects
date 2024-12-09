34c03f8c1f4e15070668a6d054047230c0baaad8
Yolanda Gunter <yemgunter@gmail.com>
2024-12-08 21:35:46 -0600

    Create Proj11Runner.java
    Pull object coding from driver
    1st run

---
+++ CLO-2/MLO-2.6/Proj11Runner.java
@@ -0,0 +1,16 @@
+public class Proj11Runner {
+
+    public void run(Object[][] array) {
+        int count = 1; // start with 1
+
+        // began loop for each row (lines 41-48 Proj11.java)
+        for (int i = 0; i < 3; i++) { // line 42
+
+            // began loop for each column in those rows
+            for (int j = 0; j < vals[i]; j++) { // line 43
+                array[i][j] + " "; // line 44
+
+            } // end loop for column
+        } // end loop for row
+    } // end method for array
+} // end class
