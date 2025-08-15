cef300e709af869715d566fe4cfa05bc0e828c27
Yolanda Gunter <yemgunter@gmail.com>
2025-08-15 09:57:37 -0500

    Update repos
    Update repo from ACC Fall 2024 - ITSE 2321 Intermediate Java Course

--- CLO-2/MLO-2.7/Proj12Runner.java
+++ CLO-2/MLO-2.7/Proj12Runner.java
@@ -1,5 +1,10 @@
 public class Proj12Runner {
-    // Empty class, no implementation required
+    public Proj12Runner() {
+        System.out.println(
+                "I certify that this program is my own work\n" +
+                        "and is not the work of others. I agree not\n" +
+                        "to share my solution with others.\n" +
+                        "Yolanda Gunter\n");
+    }// end Certification
 
-    
-}
+}// end class
